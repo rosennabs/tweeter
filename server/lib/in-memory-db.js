28fcd82c9f66d611e1556ed5926e482ea51e4d31
Rose Francis <rosennabs@gmail.com>
2023-08-03 22:17:34 -0600

    Add the Bungee & Source san 3 font styles

--- server/lib/in-memory-db.js
+++ server/lib/in-memory-db.js
@@ -8,3 +8,12 @@ const db = {
 
 module.exports = db;
 
+/* db = {
+tweets: [ { User: {}, Content: {}, Created: number }, , , , { User2: {}, Content2: {}, Created2: number } ]
+} */
+
+/* 
+-db is an object with one key named tweets
+-tweets has its values in an array, two objects as elements
+-each object element has 3 keys, 2 with objects as values
+*/
